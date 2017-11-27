ab85632fe12c615f76a945e08f19f1a4eada814e
lorejico <lorejico02@gmail.com>
2017-11-27 23:05:46 +0100

    Añadida llamada a uno de los servicios

--- mi_plugin/popup.js
+++ mi_plugin/popup.js
@@ -2,8 +2,21 @@
 Hay declarar su página de fondo (event.js) y los permisos relativos en manifest.json
 */
 
+
+function callingWebService(selectedText) {
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.open('GET', 'http://sesat.fdi.ucm.es:8080/servicios/rest/definicion/json/' + selectedText);
+    //xmlHttpRequest.open('GET', 'http://sesat.fdi.ucm.es:8080/servicios/rest/pictograma/palabra/' + selectedText);
+
+    xmlHttpRequest.onload = function() {
+        alert(xmlHttpRequest.responseText);
+    };
+    xmlHttpRequest.send();
+
+}
 function onPageDetailsReceived(details) {
-	document.getElementById('output').innerText = details.summary;
+    document.getElementById('output').innerText = details.summary;
+    callingWebService(details.summary)
 }
 // When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
@@ -12,8 +25,7 @@ window.addEventListener('load', function(evt) {
         // Call the getPageInfo function in the event page, passing in 
         // our onPageDetailsReceived function as the callback. This injects 
         // content.js into the current tab's HTML
-        var selectedText = onPageDetailsReceived;
-        //eventPage.getPageDetails(onPageDetailsReceived);
-        eventPage.getPageDetails(selectedText);
+        eventPage.getPageDetails(onPageDetailsReceived);
+        // The XMLHttpRequest API is asynchronous
     });
 });
