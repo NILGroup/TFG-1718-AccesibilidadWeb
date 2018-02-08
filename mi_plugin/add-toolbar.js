e0074ac8ec5e767ec81c56de1a51272a9b159499
lorejico <lorejico02@gmail.com>
2018-02-08 19:00:13 +0100

    Estilos separados del código

--- mi_plugin/add-toolbar.js
+++ mi_plugin/add-toolbar.js
@@ -1,27 +1,10 @@
 var height = '40px';
 var iframe = document.createElement('iframe');
+iframe.setAttribute("id", "customToolbarMenu");
 iframe.src = chrome.extension.getURL('toolbar.html');
-iframe.style.height = height;
-iframe.style.width = '100%';
-iframe.style.position = 'fixed';
-iframe.style.background = '#949494';
-iframe.style.top = '0';
-iframe.style.left = '0';
-iframe.style.zIndex = '938089';
 
 document.documentElement.appendChild(iframe);
 
 var bodyStyle = document.body.style;
 var cssTransform = 'transform' in bodyStyle ? 'transform' : 'webkitTransform';
 bodyStyle[cssTransform] = 'translateY(' + height + ')';
-
-/**
-var url = chrome.extension.getURL('toolbar.html');
-var height = '35px';
-var iframe = "<iframe scr='"+url+"' id='customToolbarMenu' style='height:" + height + "'></iframe>";
-
-$('html').append(iframe);
-
-$('body').css({
-  '-webkit-transform': 'translateY(' + height + ')'
-});**/
